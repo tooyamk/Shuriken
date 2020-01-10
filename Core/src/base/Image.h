b72aa72334c2e55ea86101a0f0ee2fde3b009aa1
a <a>
2020-01-11 03:12:12 +0800

    null

--- Core/src/base/Image.h
+++ Core/src/base/Image.h
@@ -18,17 +18,17 @@ namespace aurora {
 		Vec2ui32 size;
 		ByteArray source;
 
-		static bool convertFormat(const Vec2ui32& size, modules::graphics::TextureFormat srcFormat, const uint8_t* src, modules::graphics::TextureFormat dstFormat, uint8_t* dst);
-		static bool convertFormat(const Vec2ui32& size, modules::graphics::TextureFormat srcFormat, const uint8_t* src, uint32_t mipLevels, modules::graphics::TextureFormat dstFormat, uint8_t* dst);
+		static bool AE_CALL convertFormat(const Vec2ui32& size, modules::graphics::TextureFormat srcFormat, const uint8_t* src, modules::graphics::TextureFormat dstFormat, uint8_t* dst);
+		static bool AE_CALL convertFormat(const Vec2ui32& size, modules::graphics::TextureFormat srcFormat, const uint8_t* src, uint32_t mipLevels, modules::graphics::TextureFormat dstFormat, uint8_t* dst);
 
-		inline bool generateMips(modules::graphics::TextureFormat format, uint32_t mipLevels, ByteArray& dst, std::vector<void*>& dstDataPtr) const {
+		inline bool AE_CALL generateMips(modules::graphics::TextureFormat format, uint32_t mipLevels, ByteArray& dst, std::vector<void*>& dstDataPtr) const {
 			dst = ByteArray(calcMipsByteSize(size, mipLevels, calcPerPixelByteSize(format)));
 			dstDataPtr.resize(mipLevels);
 			return generateMips(format, mipLevels, (uint8_t*)dst.getSource(), dstDataPtr.data());
 		}
-		bool generateMips(modules::graphics::TextureFormat format, uint32_t mipLevels, uint8_t* dst, void** dstDataPtr) const;
+		bool AE_CALL generateMips(modules::graphics::TextureFormat format, uint32_t mipLevels, uint8_t* dst, void** dstDataPtr) const;
 
-		static uint32_t calcPerPixelByteSize(modules::graphics::TextureFormat format);
+		static uint32_t AE_CALL calcPerPixelByteSize(modules::graphics::TextureFormat format);
 
 		inline static uint32_t AE_CALL calcMipLevels(const Vec2ui32& size) {
 			return calcMipLevels(size.getMax());
@@ -49,38 +49,38 @@ namespace aurora {
 
 		static std::vector<uint32_t> AE_CALL calcMipsPixelSize(uint32_t n, uint32_t mipLevels);
 
-		inline static uint32_t calcNextMipPixelSize(uint32_t n) {
+		inline static uint32_t AE_CALL calcNextMipPixelSize(uint32_t n) {
 			return n > 1 ? n >> 1 : 1;
 		}
-		inline static void calcNextMipPixelSize(Vec2ui32& size) {
+		inline static void AE_CALL calcNextMipPixelSize(Vec2ui32& size) {
 			size.set(calcNextMipPixelSize(size[0]), calcNextMipPixelSize(size[1]));
 		}
-		inline static void calcNextMipPixelSize(Vec3ui32& size) {
+		inline static void AE_CALL calcNextMipPixelSize(Vec3ui32& size) {
 			size.set(calcNextMipPixelSize(size[0]), calcNextMipPixelSize(size[1]), calcNextMipPixelSize(size[2]));
 		}
 
-		inline static uint32_t calcByteSize(const Vec2ui32& size, modules::graphics::TextureFormat format) {
+		inline static uint32_t AE_CALL calcByteSize(const Vec2ui32& size, modules::graphics::TextureFormat format) {
 			return calcByteSize(size.getMultiplies(), format);
 		}
-		inline constexpr static uint32_t calcByteSize(const Vec2ui32& size, uint32_t perPixelByteSize) {
+		inline constexpr static uint32_t AE_CALL calcByteSize(const Vec2ui32& size, uint32_t perPixelByteSize) {
 			return calcByteSize(size.getMultiplies(), perPixelByteSize);
 		}
-		inline static uint32_t calcByteSize(uint32_t numPixels, modules::graphics::TextureFormat format) {
+		inline static uint32_t AE_CALL calcByteSize(uint32_t numPixels, modules::graphics::TextureFormat format) {
 			return calcByteSize(numPixels, calcPerPixelByteSize(format));
 		}
-		inline constexpr static uint32_t calcByteSize(uint32_t numPixels, uint32_t perPixelByteSize) {
+		inline constexpr static uint32_t AE_CALL calcByteSize(uint32_t numPixels, uint32_t perPixelByteSize) {
 			return numPixels * perPixelByteSize;
 		}
 
-		static uint32_t calcMipsByteSize(const Vec2ui32& size, uint32_t mipLevels, uint32_t perPixelByteSize);
-		static uint32_t calcMipsByteSize(const Vec3ui32& size, uint32_t mipLevels, uint32_t perPixelByteSize);
+		static uint32_t AE_CALL calcMipsByteSize(const Vec2ui32& size, uint32_t mipLevels, uint32_t perPixelByteSize);
+		static uint32_t AE_CALL calcMipsByteSize(const Vec3ui32& size, uint32_t mipLevels, uint32_t perPixelByteSize);
 
-		static void generateMips_UInt8s(const Vec2ui32& size, modules::graphics::TextureFormat format, uint32_t mipLevels, uint8_t numChannels, uint8_t* dst, void** dataPtr);
+		static void AE_CALL generateMips_UInt8s(const Vec2ui32& size, modules::graphics::TextureFormat format, uint32_t mipLevels, uint8_t numChannels, uint8_t* dst, void** dataPtr);
 
 	private:
-		static void _convertFormat_R8G8B8_R8G8B8A8(const Vec2ui32& size, const uint8_t* src, uint8_t* dst);
-		static void _convertFormat_R8G8B8A8_R8G8B8(const Vec2ui32& size, const uint8_t* src, uint8_t* dst);
+		static void AE_CALL _convertFormat_R8G8B8_R8G8B8A8(const Vec2ui32& size, const uint8_t* src, uint8_t* dst);
+		static void AE_CALL _convertFormat_R8G8B8A8_R8G8B8(const Vec2ui32& size, const uint8_t* src, uint8_t* dst);
 
-		static void _mipPixelsBlend(uint8_t* c, uint8_t numChannels, uint8_t numPixels);
+		static void AE_CALL _mipPixelsBlend(uint8_t* c, uint8_t numChannels, uint8_t numPixels);
 	};
 }
