1b03da66bdc5ca22807068a649b397a43ffaa621
a <a>
2021-04-08 16:32:18 +0800

    null

--- External/libpng/patch.cmake
+++ External/libpng/patch.cmake
@@ -2,5 +2,5 @@ execute_process(COMMAND git -C ${GIT_ROOT} clean -xfd)
 execute_process(COMMAND git -C ${GIT_ROOT} reset --hard)
 
 file(READ ${CMAKE_LISTS_FILE} content)
-string(REPLACE "set(PNG_LIB_NAME png\${PNGLIB_MAJOR}\${PNGLIB_MINOR})" "set(PNG_LIB_NAME png)" content ${content})
-file(WRITE ${CMAKE_LISTS_FILE} ${content})
+string(REPLACE "set(PNG_LIB_NAME png\${PNGLIB_MAJOR}\${PNGLIB_MINOR})" "set(PNG_LIB_NAME png)" content "${content}")
+file(WRITE ${CMAKE_LISTS_FILE} "${content}")
