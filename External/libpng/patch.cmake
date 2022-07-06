eceb9b1b570c5da61f6e9d117e558dd3d14fc121
null <null>
2022-07-06 14:00:26 +0800

    null

--- External/libpng/patch.cmake
+++ External/libpng/patch.cmake
@@ -2,5 +2,13 @@ execute_process(COMMAND git -C ${GIT_ROOT} clean -xfd)
 execute_process(COMMAND git -C ${GIT_ROOT} reset --hard)
 
 file(READ ${CMAKE_LISTS_FILE} content)
+
 string(REPLACE "set(PNG_LIB_NAME png\${PNGLIB_MAJOR}\${PNGLIB_MINOR})" "set(PNG_LIB_NAME png)" content "${content}")
+
+string(REPLACE "find_program(AWK NAMES gawk awk)" "if(CMAKE_GENERATOR MATCHES \"Xcode\" AND CMAKE_XCODE_BUILD_SYSTEM MATCHES 12)\r\n    set(XCODE_NEW_BUILD_SYSTEM True)\r\nelse()\r\n  set(XCODE_NEW_BUILD_SYSTEM False)\r\n   find_program(AWK NAMES gawk awk)\r\nendif()" content "${content}")
+string(REPLACE "if(NOT AWK OR ANDROID)" "if(XCODE_NEW_BUILD_SYSTEM OR NOT AWK OR ANDROID)" content "${content}")
+
+string(REPLACE "if(CYGWIN OR MINGW)" "if(False)" content "${content}")
+string(REPLACE "if(NOT WIN32)" "if(False)" content "${content}")
+
 file(WRITE ${CMAKE_LISTS_FILE} "${content}")
--- Tools/macOS/configure.sh
+++ Tools/macOS/configure.sh
@@ -2,4 +2,4 @@
 rm -f ./CMakeCache.txt
 c=`which clang`
 cxx=`which clang++`
-cmake -DSRK_TESTS=ON -DCMAKE_INSTALL_PREFIX="install" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$c -DCMAKE_CXX_COMPILER=$cxx -G "Xcode" ..
+cmake -DSRK_TESTS=ON -DCMAKE_INSTALL_PREFIX="install" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=$c -DCMAKE_CXX_COMPILER=$cxx -G Xcode ..
