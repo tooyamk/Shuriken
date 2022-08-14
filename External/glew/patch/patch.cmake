c6080a6f1e5fff620505b8ea49327b882aa0dbe8
null <null>
2022-08-14 16:06:42 +0800

    null

--- External/glew/patch/patch.cmake
+++ External/glew/patch/patch.cmake
@@ -1,10 +1,12 @@
-execute_process(COMMAND git -C ${SRC_DIR} clean -xfd)
-execute_process(COMMAND git -C ${SRC_DIR} reset --hard)
+execute_process(COMMAND git -C ${PROJ_BIN_DIR}/src clean -xfd)
+execute_process(COMMAND git -C ${PROJ_BIN_DIR}/src reset --hard)
 
-set(file ${SRC_DIR}/build/cmake/CMakeLists.txt)
+set(file ${PROJ_BIN_DIR}/src/build/cmake/CMakeLists.txt)
 file(READ ${file} content)
 string(REPLACE "set (GLEW_LIB_NAME glew32)" "set (GLEW_LIB_NAME glew)" content "${content}")
 string(REPLACE "set (GLEW_LIB_NAME GLEW)" "set (GLEW_LIB_NAME glew)" content "${content}")
 file(WRITE ${file} "${content}")
 
-file(COPY ${PATCH_DIR}/src/ DESTINATION ${SRC_DIR})
+file(ARCHIVE_EXTRACT INPUT ${CMAKE_CUR_SRC_DIR}/patch/files.zip DESTINATION ${PROJ_BIN_DIR}/tmp)
+file(COPY ${PROJ_BIN_DIR}/tmp/src/ DESTINATION ${PROJ_BIN_DIR}/src)
+file(REMOVE_RECURSE INPUT ${PROJ_BIN_DIR}/tmp/src)
