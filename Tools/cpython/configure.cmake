f7af84bf09def50f3cbda4a2131e2933f162fb03
null <null>
2023-04-20 18:54:43 +0800

    null

--- Tools/cpython/configure.cmake
+++ Tools/cpython/configure.cmake
@@ -1,3 +1,3 @@
 if (SRK_HOST_OS_LINUX OR SRK_HOST_OS_MACOS)
-    execute_process(COMMAND ${SRC_DIR}/configure --enable-optimizations --prefix=${INSTALL_DIR} WORKING_DIRECTORY ${BUILD_DIR} RESULT_VARIABLE err)
+    execute_process(COMMAND ${SRC_DIR}/configure --enable-optimizations --prefix=${INSTALL_DIR} CPPFLAGS='-I${ZLIB_INCLUDE}' LDFLAGS='-L${ZLIB_DIR}' WORKING_DIRECTORY ${BUILD_DIR} RESULT_VARIABLE err)
 endif ()
