f970b72ab38c322919ba24e326c47ebe1c266a6e
tooyamk <0xff000000@gmail.com>
2023-09-01 23:40:57 +0800

    null

--- External/zstd/patch.cmake
+++ External/zstd/patch.cmake
@@ -3,6 +3,7 @@ execute_process(COMMAND git -C ${SRC_DIR} reset --hard)
 
 set(file ${SRC_DIR}/build/cmake/lib/CMakeLists.txt)
 file(READ ${file} content)
+string(REPLACE "set_source_files_properties(\${Sources} PROPERTIES LANGUAGE C)" "if(NOT CMAKE_ASM_COMPILER STREQUAL CMAKE_C_COMPILER)\r\nset_source_files_properties(\${Sources} PROPERTIES LANGUAGE C)\r\nendif()" content "${content}")
 string(REPLACE "VERSION \${zstd_VERSION_MAJOR}.\${zstd_VERSION_MINOR}.\${zstd_VERSION_PATCH}" "" content "${content}")
 string(REPLACE "SOVERSION \${zstd_VERSION_MAJOR}" "" content "${content}")
 file(WRITE ${file} "${content}")
